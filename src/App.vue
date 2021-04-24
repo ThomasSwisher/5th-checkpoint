bb8a83f3398cd9288062e77d4167887e83382bec
Thomas Swisher <thomas@structuredseo.com>
2021-04-24 06:46:05 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <div class="row flex-grow-1">
+      <div class="col-3">
+        <LeftSidebar />
+      </div>
+      <div class="col-6">
+        <router-view />
+      </div>
+      <div class="col-3">
+        RightSidebar
+      </div>
+    </div>
+  </main>
+  <footer>
+    <div class="bg-dark text-light text-center p-4">
+      Made with Bullets
+    </div>
+  </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+export default {
+  name: 'App',
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+</style>
---
+++ src/components/LeftSidebar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="left-sidebar">
+    <div class="side-bar-container">
+      <img class="rounded-circle" :src="state.user.picture" alt="Creator image">
+      <p>{{ state.user.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, reactive } from 'vue'
+import { AppState } from '../AppState'
+export default {
+  name: 'LeftSidebar',
+  setup() {
+    //    const route = useRoute()
+    const state = reactive({
+      account: computed(() => AppState.account),
+      user: computed(() => AppState.user)
+    })
+    return { state }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 20vh;
+  margin-top: 2vh;
+}
+
+.side-bar-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  width: 200px;
+  margin-top: 25px;
+  min-height: 80vh;
+}
+
+.left-sidebar {
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+}
+
+</style>
